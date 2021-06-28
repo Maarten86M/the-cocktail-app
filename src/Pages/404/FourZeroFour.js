77d52efad2c99c27d558e6b47e7c3cea60d79d7e
Maarten86M <maartenmoelands@gmail.com>
2021-06-28 12:12:15 +0200

    Good starting point for the mobile responsive styling

--- src/Pages/404/FourZeroFour.js
+++ src/Pages/404/FourZeroFour.js
@@ -6,9 +6,9 @@ function FourZeroFour() {
     return (
             <div className="fourzerofour-page">
                 <img className="fourzerofour" src={fourzerofour} alt="404"/>
-                <h1>Oops! Sorry we can't find that page! </h1>
+                <h1 className="oops-text">Oops! Sorry we can't find that page! </h1>
             </div>
     )
-}
+};
 
 export default FourZeroFour;
